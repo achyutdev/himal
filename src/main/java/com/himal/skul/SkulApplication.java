13e98dd5bccca6cfe0c601c1a95ea4065a5390bd
Achyut Dev <mail_me@achyutdev.me>
2017-05-22 20:33:36 -0400

    Initial commit

---
+++ src/main/java/com/himal/skul/SkulApplication.java
@@ -0,0 +1,12 @@
+package com.himal.skul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkulApplication.class, args);
+	}
+}
